f853a33bd9a62edbb4b21cd85fae017428128df5
Tyan <52327619+tianxk-hit@users.noreply.github.com>
2024-05-28 14:51:05 +0800

    Add files via upload

--- Chapter6/Sim/openmips_min_sopc_tb.v
+++ Chapter6/Sim/openmips_min_sopc_tb.v
@@ -1,22 +1,27 @@
-`include "../Include/define.v"
+module openmips_min_sopc_tb();
 
-module openmips_min_sopc_tb ();
-    reg clk_50;
-    reg rst;
+`include	"defines.v"
 
-    initial begin
-        clk_50 = 1'b0;
-        forever #10 clk_50 = ~clk_50;
-    end
+reg		CLOCK_50;
+reg		rst;
 
-    initial begin
-        rst = `ResetEnable;
-        #195 rst = `ResetDisable;
-        #1000 $stop;
-    end
+//每隔10ns，CKOCK_50信号翻转一次，所以一个周期是20ns，对应50MHz
+initial begin
+	CLOCK_50 = 1'b0;
+	forever	#10 CLOCK_50 = ~CLOCK_50;
+end
 
-    openmips_min_sopc openmips_min_sopc_inst0(
-        .clk(clk_50),
-        .rst(rst)
-    );
+//最初时刻，复位信号有效，在第195ns，复位信号有效，最小SOPC开始运行
+initial begin
+	rst = `RstEnable;
+	#195 	rst = `RstDisable;
+	#1000	$stop;
+end
+
+//例化最小SOPC
+openmips_min_sopc openmips_min_sopc0(
+.clk(CLOCK_50),
+.rst(rst)
+);
+
 endmodule
